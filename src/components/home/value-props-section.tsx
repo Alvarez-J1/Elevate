14242be458e4c74d4437cc88622728b93d257e98
Joel Alvarez <Joel124@outlook.es>
2026-08-15 08:27:24 -0400

    Label value props section

--- src/components/home/value-props-section.tsx
+++ src/components/home/value-props-section.tsx
@@ -31,11 +31,12 @@ const valueProps = [
 
 export function ValuePropsSection() {
   return (
-    <section className="home-section">
+    <section aria-labelledby="value-props-heading" className="home-section">
       <Container className="min-[1600px]:max-w-[min(90vw,1760px)] min-[1600px]:px-12 min-[1920px]:max-w-[min(92vw,2400px)] min-[1920px]:px-16">
         <FadeIn>
           <SectionHeader
             eyebrow="Why Elevate"
+            titleId="value-props-heading"
             title="Why shop Elevate?"
           />
         </FadeIn>
